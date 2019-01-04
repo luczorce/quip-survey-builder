bcbc68a2e732aea6639f444f1aabe916c951fc58
Ana Luczynski <ana.luczynski@salesforce.com>
2019-01-04 16:56:12 -0600

    update styles

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,5 +1,6 @@
 import { qatypes } from '../util/enums.js';
 import Style from "./Form.less";
+import MainStyle from "../App.less";
 
 export default class SelectQ extends React.Component {
   static propTypes = {
@@ -55,14 +56,14 @@ export default class SelectQ extends React.Component {
 
   render() {
     const options = this.props.optionsList.options.map(o => {
-      return <li key={o.guid}>
-        <input value={o.value} onChange={(event) => this.questionOptionsUpdate(event.target.value, o.guid)} />
+      return <li key={o.guid} className={Style.formOption}>
+        <input value={o.value} type="text" onChange={(event) => this.questionOptionsUpdate(event.target.value, o.guid)} />
         <quip.apps.ui.Button type="button" onClick={() => this.removeOption(o.guid)} disabled={this.props.lock} text="remove" />
       </li>;
     });
 
     return <li key={this.props.guid} className={Style.formSection}>
-      <p>
+      <p className={Style.sectionDescription}>
         select box <em>(for a dropdown of many available answers)</em>
         <button type="button" onClick={this.deleteQuestion} className={Style.sectionDeleter} disabled={this.props.lock}>delete question</button>
       </p>
@@ -72,8 +73,8 @@ export default class SelectQ extends React.Component {
         <input type="text" value={this.props.question} placeholder="(Who did you talk to last?)" onChange={this.questionValueUpdate} disabled={this.props.lock} />
       </label>
 
-      <p>
-        options
+      <p className={MainStyle.flexirow}>
+        <span>options</span>
         <quip.apps.ui.Button type="button" onClick={this.questionOptionsAdd} disabled={this.props.lock} text="add option" />
       </p>
 
