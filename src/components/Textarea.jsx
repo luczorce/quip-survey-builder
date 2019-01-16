7111ebcfbc08644b68855f78f78c06634d5c950f
Ana Luczynski <ana.luczynski@salesforce.com>
2019-01-16 16:18:57 -0600

    update textarea question to include helper text

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -4,6 +4,7 @@ import Style from "./Form.less";
 export default class TextareaQ extends React.Component {
   static propTypes = {
     question: React.PropTypes.string,
+    helper: React.PropTypes.string,
     guid: React.PropTypes.number,
     updated: React.PropTypes.func,
     updateOrder: React.PropTypes.func,
@@ -18,6 +19,7 @@ export default class TextareaQ extends React.Component {
   moveQuestionDown = () => {
     let question = {
       question: this.props.question,
+      helper: this.props.helper,
       guid: this.props.guid,
       type: qatypes.textarea
     };
@@ -28,6 +30,7 @@ export default class TextareaQ extends React.Component {
   moveQuestionUp = () => {
     let question = {
       question: this.props.question,
+      helper: this.props.helper,
       guid: this.props.guid,
       type: qatypes.textarea
     };
@@ -35,9 +38,21 @@ export default class TextareaQ extends React.Component {
     this.props.updateOrder(question, true);
   }
 
+  questionHelperValueUpdate = (event) => {
+    let updatedQuestion = {
+      question: this.props.question,
+      helper: event.target.value,
+      guid: this.props.guid,
+      type: qatypes.textarea
+    };
+
+    this.props.updated(updatedQuestion);
+  }
+
   questionValueUpdate = (event) => {
     let updatedQuestion = {
       question: event.target.value,
+      helper: this.props.helper,
       guid: this.props.guid,
       type: qatypes.textarea
     };
@@ -55,6 +70,11 @@ export default class TextareaQ extends React.Component {
       <label className={Style.formInput}>
         <span>question</span>
         <input type="text" value={this.props.question} placeholder="(Who did you talk to last?)" onChange={this.questionValueUpdate} disabled={this.props.lock} />
+      </label>
+
+      <label className={Style.formInput}>
+        <span>optional helper text</span>
+        <input type="text" value={this.props.helper} placeholder="(new lines for each person, include their title: Mary Smith, CEO of Kola Co.)" onChange={this.questionHelperValueUpdate} disabled={this.props.lock} />
       </label>
 
       <p className={Style.sectionFooter}>
