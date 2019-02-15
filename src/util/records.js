cb2ed8963e606a13e178652e519b87194479d255
Ana Luczynski <ana.luczynski@salesforce.com>
2019-02-15 10:50:14 -0600

    clean up the root file of the record types, move them to their own file

---
+++ src/util/records.js
@@ -0,0 +1,32 @@
+export class SurveyRecord extends quip.apps.RootRecord {
+  static getProperties() {
+    return {
+      purpose: 'string',
+      surveyName: 'string',
+      surveyId: 'number',
+      questions: 'array',
+      questionOptions: quip.apps.RecordList.Type(OptionsRecord),
+      answers: 'array'
+    };
+  }
+
+  static getDefaultProperties() {
+    return {
+      questionOptions: []
+    };
+  }
+}
+
+export class OptionsRecord extends quip.apps.Record {
+  static getProperties() {
+    return {
+      // this will relate to the question guid, from the Survey RootRecord
+      // we use this guid to tie the options back to the specific question
+      guid: 'number',
+      // this will contain the data on the options available to the question
+      // while the survey is being built, this is all stored in the quip records
+      // it will look like [ {guid: 'number', value: 'string', helper: 'string'}, ...]
+      options: 'array'
+    };
+  }
+}
