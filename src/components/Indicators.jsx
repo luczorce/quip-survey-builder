aa6870dc96ba89caa3468f48d6e05f914cc69383
Ana Luczynski <ana.luczynski@salesforce.com>
2019-02-26 15:30:54 -0600

    update the saved icon

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,11 +1,13 @@
-import Style from "../App.less";
+import Style from '../App.less';
 
 export default class SavedIcon extends React.Component {
   // TODO how to detect "dirty" data?
   render() {
-    return <span>
-      <span title="saved to server" aria-hidden="true">&#x2713;</span>
+    return <span style={{float: "right"}}>
+      <span title="saved to server" aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#327227" strokeWidth="4" strokeLinecap="round" strokeLinejoin="arcs"><polyline points="20 6 9 17 4 12"></polyline></svg>
+      </span>
       <span className={Style.visuallyHidden}>saved to server</span>
-    </span>
+    </span>;
   }
 }
