c7dc398c21816d2e44d9667b7ebfa6aa6bf66a07
Ana Luczynski <ana.luczynski@salesforce.com>
2019-02-27 15:55:06 -0600

    change color of the 'saved to server' icon

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -6,7 +6,7 @@ export default class SavedIcon extends React.Component {
   render() {
     return <span className={FormStyle.icon}>
       <span title="saved to server" aria-hidden="true">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#327227" strokeWidth="4" strokeLinecap="round" strokeLinejoin="arcs"><polyline points="20 6 9 17 4 12"></polyline></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#5c6470" strokeWidth="4" strokeLinecap="round" strokeLinejoin="arcs"><polyline points="20 6 9 17 4 12"></polyline></svg>
       </span>
       <span className={Style.visuallyHidden}>saved to server</span>
     </span>;
